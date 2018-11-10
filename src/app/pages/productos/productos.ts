52161c173a2a792db25d0699a10b9d74dd946497
Javier Rojas <javierrojas6@gmail.com>
2018-11-10 17:06:10 -0500

    finalmente funciona io4

--- src/app/pages/productos/productos.ts
+++ src/app/pages/productos/productos.ts
@@ -2,13 +2,22 @@ import { HomePage } from './../home/home';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'page-productos',
   templateUrl: 'productos.html'
 })
 export class ProductosPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    private http: HttpClient
+  ) {
+    this.http.get('http://localhost:3000/product/get/all')
+      .subscribe(response => {
+        console.log(response);
+      });
   }
 
   openPage(page) {
