60dd971ecc1c3275be8e74d654f14c2dad114eb4
Javier Rojas <javierrojas6@gmail.com>
2018-11-14 19:30:19 -0500

    front funcionando

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
 
+export class HomePage {
+  constructor(
+    public navCtrl: NavController,
+    private http: HttpClient
+  ) {
+    this.http.get('http://localhost:3000/product/get/all')
+      .subscribe(as => {
+        console.log(as);
+      });
+
+  }
 }
